a67c2e4af4f49f8fd81c0d59217a12bc313770de
Legend <89526133+1337Legend@users.noreply.github.com>
2022-04-28 22:39:30 +0300

    Update mpctyload.lua

--- mpctyload.lua
+++ mpctyload.lua
@@ -1,7 +1,7 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
 if game.CreatorId == 123247 then
-local loaderurl = "https://raw.githubusercontent.com/1337Legend/asdjkalsdja/main/mpctyload"
-local scripturl = "https://raw.githubusercontent.com/1337Legend/asdjkalsdja/main/mpcty"
+local loaderurl = "https://raw.githubusercontent.com/1337Legend/asdjkalsdja/main/mpctyload.lua"
+local scripturl = "https://raw.githubusercontent.com/1337Legend/asdjkalsdja/main/mpcty.lua"
 loadstring(game:HttpGet(scripturl,true))()
 syn.queue_on_teleport([[
     wait(3)
